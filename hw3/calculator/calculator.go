b856cfd9ff54f30904520af0a7a3e97a3dcb94d6
simon <trishichkin@mail.ru>
2021-08-15 06:47:49 +0300

    HW3 - calculator, simple
    Доработать калькулятор: больше операций и валидации данных.
    Написать приложение, которое ищет все простые числа от 0 до N включительно. Число N должно быть задано из стандартного потока ввода.

---
+++ hw3/calculator/calculator.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Введите число a:")
+	var a float64
+	_, err := fmt.Scan(&a)
+
+	if err != nil {
+		fmt.Println("Введите число")
+		return
+	}
+
+	fmt.Println("Введите число b:")
+	var b float64
+	_, err = fmt.Scan(&b)
+
+	if err != nil {
+		fmt.Println("Введите число")
+		return
+	}
+
+	fmt.Println("Выберите операцию: +, -, *, /")
+
+	var operation string
+
+	_, err = fmt.Scanln(&operation)
+
+	if err != nil {
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println("Результат:", a+b)
+	case "-":
+		fmt.Println("Результат:", a-b)
+	case "*":
+		fmt.Println("Результат:", a*b)
+	case "/":
+		if b != 0 {
+			fmt.Println("Результат:", a/b)
+		} else {
+			fmt.Println("Деление на 0")
+		}
+	default:
+		fmt.Println("Неизвестная операция")
+	}
+}
---
+++ hw3/simple/simple.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Введите число N:")
+	var n int
+	_, err := fmt.Scan(&n)
+
+	if err != nil {
+		fmt.Println("Введите число")
+		return
+	}
+
+	simples := getSimpleNumbers(n)
+	for _, p := range simples {
+		fmt.Println(p)
+	}
+}
+
+func getSimpleNumbers(N int) (primes []int) {
+	b := make([]bool, N)
+	primes = append(primes, 0)
+	primes = append(primes, 1)
+
+	for i := 2; i < N; i++ {
+		if b[i] {
+			continue
+		}
+		primes = append(primes, i)
+		for k := i * i; k < N; k += i {
+			b[k] = true
+		}
+	}
+
+	return
+}
