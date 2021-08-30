e53fc330ceb4fbdb56fca3791a0589234f47dead
simon <trishichkin@mail.ru>
2021-08-30 05:30:16 +0300

    HW4 - исправление  ошибок

--- hw4/insertion/insertion.go
+++ hw4/insertion/insertion.go
@@ -10,10 +10,8 @@ func main() {
 func insertion(arr []int) []int {
 	for i := 1; i <= len(arr)-1; i++ {
 		x := arr[i]
-		j := i
-		for j > 0 && arr[j-1] > x {
+		for j := i; j > 0 && arr[j-1] > x; j-- {
 			arr[j], arr[j-1] = arr[j-1], x
-			j--
 		}
 	}
 	return arr
